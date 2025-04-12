249a99b64d4130799b3ae2ba632e0d9f82d9d3f1
Marcelle Pereira Nunes <marcelle.nunes2003@gmail.com>
2025-04-12 18:55:47 -0300

    Update dashboardController

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -9,36 +9,31 @@ class DashboardController extends Controller
 
     public function index()
     {
-        // Obtém o id da conta do usuário autenticado
         $id_conta = auth()->user()->id_conta;
 
-        // Total de clientes ativos
         $total_clientes = \DB::table('customers')
             ->where('id_conta', $id_conta)
             ->where('status', 'active')
             ->count();
 
-        // Total de produtos ativos
         $total_produtos = \DB::table('products')
             ->where('id_conta', $id_conta)
             ->where('status', 'active')
             ->count();
 
-        // Contar os clientes por mês
         $clientsByMonth = \DB::table('customers')
         ->select(\DB::raw('MONTH(created_at) as month'), \DB::raw('YEAR(created_at) as year'), \DB::raw('COUNT(*) as total_clients'))
         ->where('id_conta', $id_conta)
         ->where('status', 'active')
         ->groupBy(\DB::raw('YEAR(created_at), MONTH(created_at)'))
-        ->orderBy(\DB::raw('YEAR(created_at), MONTH(created_at)'))  // Corrigido: Remover a palavra 'ASC' redundante
+        ->orderBy(\DB::raw('YEAR(created_at), MONTH(created_at)'))
         ->get();
-        // Preparar os dados para o gráfico
+
         foreach ($clientsByMonth as $row) {
-            $months[] = $row->month . '/' . $row->year; // Formato: Mês/Ano
+            $months[] = $row->month . '/' . $row->year;
             $clientCounts[] = $row->total_clients;
         }
 
-        // Passa as variáveis para a view
         return view('dashboard.index', compact('total_clientes', 'total_produtos', 'months', 'clientCounts'));
     }
 }
